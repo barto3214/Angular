b01d9cb7781f962d7dbe1980871007cb04b3c9d5
barto3214 <bartek.slusarz@onet.pl>
2025-09-10 11:38:06 +0200

    Next things in angular

--- projekt_kl4/src/app/app.component.ts
+++ projekt_kl4/src/app/app.component.ts
@@ -19,4 +19,43 @@ export class AppComponent {
   onButtonClick() {
     this.kolor = this.kolor === "yellow" ? "green" : "yellow";
   }
+  isHighlighted = true;
+  isDisabled = true;
+  loggedIn = false;
+  isadmin = false;
+
+  jakiklawisz(event : KeyboardEvent) {
+    console.log("Wciśnięto klawisz: " + event.key);
+  }
+
+  jestenter(){
+    console.log("Wciśnięto enter");
+  }
+
+  jestenter2(){
+    console.log("Wciśnięto shift + enter");
+  }
+
+  login(){
+    this.loggedIn = true;
+  }
+
+  // obiekt literalny
+  user={
+    name: 'Jan Kowalski',
+    age: 30
+    }
+
+  //lista
+  owoce = ['jabłko', 'banan', 'gruszka'];
+  
+  //tablica obiektów
+  samochody = [
+    { marka: 'Toyota', model: 'Corolla', rok: 2020 },
+    { marka: 'Honda', model: 'Civic', rok: 2019 },
+    { marka: 'Ford', model: 'Focus', rok: 2018 }
+  ];
+
+  //tablica liczb
+  numbers = [1, 2, 3, 4, 5];
 }
